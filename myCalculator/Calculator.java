df2071bfb6b45ae9e7fcc7c6ad6d18ba03aa020b
mxtimu <matimujunior3@gmail.com>
2024-11-27 12:18:01 +0200

    version 1

---
+++ myCalculator/Calculator.java
@@ -0,0 +1,17 @@
+package myCalculator;
+
+public class Calculator {
+    //First the Instances
+    int number;
+    Calculator(){
+
+    }
+    public int add(int a, int b){
+        return a + b;
+    }
+    public int subtract(int a, int b){
+        return a - b;
+    }
+
+
+}
---
+++ myCalculator/Main.java
@@ -0,0 +1,9 @@
+package myCalculator;
+
+public class Main {
+    public static void main (String[] args){
+    Calculator example1 =  new Calculator();
+    System.out.println(example1.add(100 , 50));
+
+    }
+}
